d514466ddc0831f134097f58a7be61af06e4da50
Ajinkya Borade <ajinkyax@pm.me>
2024-03-21 21:04:01 +0530

    merge same row tiles on LEFT key

--- src/user_input.rs
+++ src/user_input.rs
@@ -13,6 +13,7 @@ impl Plugin for UserInputPlugin {
 }
 
 fn update_user_input(
+  mut commands: Commands,
   keyboard_input: Res<ButtonInput<KeyCode>>,
   mut tiles: Query<(Entity, &mut TilePosition, &mut Tile)>
 ) {
@@ -22,14 +23,50 @@ fn update_user_input(
   
   match user_input {
     Some(UserInput::Left) => {
-      dbg!("left");
-      let mut it = tiles.iter_mut().sorted_by(|a, b| {
-        match Ord::cmp(&a.1.y, &b.1.y) {
-          Ordering::Equal => Ord::cmp(&a.1.x, &b.1.x), // if Equal means 2 tiles are in a row
-          ordering => ordering
+      let mut it = tiles.iter_mut()
+        .sorted_by(|a, b| {
+          match Ord::cmp(&a.1.y, &b.1.y) {
+            Ordering::Equal => Ord::cmp(&a.1.x, &b.1.x), // if Equal means 2 tiles are in a row
+            ordering => ordering
+          }
+        })
+        .peekable();
+      // dbg!(it.collect::<Vec<_>>());
+      
+      let mut column: u32 = 0;
+      // loop over all tiles from bottom-left to right (next column)
+      // then move to next row and go column-wise
+      // if tiles are in same row we merge them, while despawn the 2nd tile
+      while let Some(mut tile) = it.next() {
+        tile.1.x = column;// move tile to left most
+        match it.peek() {
+          None => {},
+          Some(tile_next) => {
+            // check y rows
+            if tile_next.1.y != tile.1.y {
+              column = 0; // dont merge
+            }
+            // check values
+            else if tile_next.2.points != tile.2.points {
+              column += 1;// different values, dont merge
+            }
+            // all case match, merge
+            else {
+              let real_next_tile = it.next().expect("Tile should have been there!");
+              tile.2.points += real_next_tile.2.points;
+              commands.entity(real_next_tile.0).despawn_recursive();
+              
+              if let Some(next_next_tile) = it.peek() {
+                if next_next_tile.1.y != tile.1.y {
+                  column = 0;
+                } else {
+                  column += 1;
+                }
+              }
+            }
+          }
         }
-      });
-      dbg!(it.collect::<Vec<_>>());
+      }
     }
     Some(UserInput::Right) => {
       dbg!("right");
